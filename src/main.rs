b68a356987715c90d637ca67d17a961455f27415
anjiboss <nhu.nhu.face@gmail.com>
2022-11-07 04:49:35 +0900

    working with choices and buttons

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use cursive::views::TextView;
+use cursive::{views::Dialog, Cursive};
 
 fn main() {
     let mut siv = cursive::default();
 
     siv.add_global_callback('q', |s| s.quit());
 
-    siv.add_layer(TextView::new("Hello cursive! Press <q> to quit."));
+    siv.add_layer(
+        Dialog::text("Hello this is diaglog")
+            .title("This is title")
+            .button("Quit", |s| s.quit())
+            .button("Next", show_next),
+    );
 
     siv.run();
+}
+
+fn show_next(siv: &mut Cursive) {
+    siv.pop_layer();
+    siv.add_layer(
+        Dialog::text("Choose yes or no")
+            .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
+            .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
+            .button("Uh?", |s| s.add_layer(Dialog::info("Try again"))),
+    )
+}
+
+fn show_answer(s: &mut Cursive, msg: &str) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text(msg)
+            .title("Results")
+            .button("Finish", |s| s.quit()),
+    );
 }
