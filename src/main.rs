bb2f0b324e6a88bf159e471fa5946bfebed723a7
anjiboss <nhu.nhu.face@gmail.com>
2022-11-07 05:09:47 +0900

    add theme

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use cursive::{views::Dialog, Cursive};
+use cursive::{
+    theme::{BorderStyle, Palette},
+    views::Dialog,
+    Cursive, With,
+};
 
 fn main() {
     let mut siv = cursive::default();
+    siv.set_theme(cursive::theme::Theme {
+        shadow: true,
+        borders: BorderStyle::Simple,
+        palette: Palette::default().with(|palette| {
+            use cursive::theme::BaseColor::*;
+            use cursive::theme::Color::*;
+            use cursive::theme::PaletteColor::*;
+
+            palette[Background] = TerminalDefault;
+            palette[View] = TerminalDefault;
+            palette[Primary] = White.dark();
+            palette[TitlePrimary] = Blue.light();
+            palette[Secondary] = Blue.light();
+            palette[Highlight] = Blue.dark();
+        }),
+    });
 
     siv.add_global_callback('q', |s| s.quit());
-
     siv.add_layer(
         Dialog::text("Hello this is diaglog")
             .title("This is title")
